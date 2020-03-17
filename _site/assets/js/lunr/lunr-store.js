a54e18e8b868b0fec744b47eea1170cf7b0d9ef2
NishanthJKumar <nishanth.kumar20@gmail.com>
2020-03-17 16:45:26 -0400

    Blog posts done!

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,10 +4,20 @@ var store = [{
         "tags": ["Meditations","IB"],
         "url": "https://nishanthjkumar.com/More-than-a-45-a-reflection-on-my-time-in-High-School/",
         "teaser":null},{
+        "title": "Humans, Robots And Lessons From Hri 2019",
+        "excerpt":"Over winter break, I had the opportunity to write an extended abstract (my first first-author thing!) for an idea I’d been kicking around with some other members of my lab. We ended up submitting said abstract to VAM-HRI 2019: a workshop centered on Mixed-Reality for Human-Robot Interaction. Fortunately enough, our...","categories": [],
+        "tags": ["Research","Conference"],
+        "url": "https://nishanthjkumar.com/Humans,-Robots-and-Lessons-from-HRI-2019/",
+        "teaser":null},{
         "title": "Why Robots? Deciding What I Want To Do With My Life",
         "excerpt":"Ever since my parents bought me my first robotic toy as a kid, I’ve been excited about robots. Over the years, I’ve been lucky enough to have had numerous opportunities to work with robots. I’ve played with toys, spent multiple years building Lego and heavy-metal robots for competitions, and most-recently...","categories": [],
         "tags": ["Meditations","Research"],
         "url": "https://nishanthjkumar.com/why-robots-deciding-what-I-want-to-do-with-my-life/",
+        "teaser":null},{
+        "title": "Some Thoughts And Robots From Icra 2019",
+        "excerpt":"Some weeks ago, I had the pleasure of attending the 36th International Conference on Robotics and Automation (ICRA) in Montreal, Canada. This was my first major Robotics and AI focused conference and it was probably one of the most interesting and learning-intensive weeks I’ve ever had. This post is an...","categories": [],
+        "tags": ["Research","Conference"],
+        "url": "https://nishanthjkumar.com/Some-thoughts-and-robots-from-ICRA-2019/",
         "teaser":null},{
         "title": "Innovation At Its Finest",
         "excerpt":"A while ago, I was attempting to run last-minute experiments to finish up a paper just in time for a conference deadline. Our project essentially involved getting our beautiful white robot-arm shown above to press the buttons on the panel in front of it. Unfortunately for us, our approach required...","categories": [],

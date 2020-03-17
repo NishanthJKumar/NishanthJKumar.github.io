ddf55c0cf53f2e18b971545b30cb415de9be29f8
NishanthJKumar <nishanth.kumar20@gmail.com>
2020-03-16 23:34:01 -0400

    Why robots post is transferred

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "Why Robots? Deciding What I Want To Do With My Life",
+        "excerpt":"Ever since my parents bought me my first robotic toy as a kid, I’ve been excited about robots. Over the years, I’ve been lucky enough to have had numerous opportunities to work with robots. I’ve played with toys, spent multiple years building Lego and heavy-metal robots for competitions, and most-recently...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/why-robots-deciding-what-I-want-to-do-with-my-life/",
+        "teaser":null}]
